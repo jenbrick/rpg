cab4e5c7b6125d2f199e6c5799fc864b42c19c52
jenbrick <jenbrick73@comcast.net>
2023-01-29 17:27:53 -0500

    begin work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,21 @@
 import { Component } from '@angular/core';
+import data from '../assets/json/characters.json';
+
+interface Person { 
+  name : String,
+  type : String,
+  gender : String,
+  race : String,
+  role : String,
+  specialPowers : String,
+  genres : String,
+  sources : String,
+  media : String,
+  setting : String,
+  notes : String,
+  picture : String
+}
+  
 
 @Component({
   selector: 'app-root',
@@ -6,5 +23,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'rpg';
-}
+  origPersons: Person[] = data;
+  msg : String = "Not clicked"; 
+
+  party: Person[] = this.origPersons;
+ 
+   
+  clickEvent() {
+    this.party = [];
+    this.msg = "Button is Clicked";
+    this.party[0] = this.origPersons[Math.floor(Math.random() * 1000) + 1];
+    this.party[1] = this.origPersons[Math.floor(Math.random() * 1000) + 1];
+    this.party[2] = this.origPersons[Math.floor(Math.random() * 1000) + 1];
+    this.party[3] = this.origPersons[Math.floor(Math.random() * 1000) + 1];
+    this.party[4] = this.origPersons[Math.floor(Math.random() * 1000) + 1];
+    return this.msg;
+  }
+ }
