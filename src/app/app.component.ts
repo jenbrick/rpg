c577cf665d046cc330dc32d3fdb612add47f465d
jenbrick <jenbrick73@comcast.net>
2023-02-01 16:52:01 -0500

    x

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 //ng build --output-path docs --base-href /rpg/
 import { Component } from '@angular/core';
 import data from '../assets/json/characters.json';
+import {
+  trigger, state, style, animate, transition, query, group
+ } from '@angular/animations';
 
 interface Person { 
   name : String,
@@ -14,14 +17,23 @@ interface Person {
   media : String,
   setting : String,
   notes : String,
-  picture : String
+  picture : String,
+  dynpic : String
 }
   
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(1000, style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
 
@@ -33,10 +45,16 @@ export class AppComponent {
   sliderValue = 4;
   storedValues: number[] = [];
   partySizeLimit = this.origPersons.length;
+
+  showMyClass = true;
+
+  displayStatic = false;
    
   generateParty() {
     this.party = [];
     this.storedValues = [];
+    this.showMyClass = true;
+    this.displayStatic = false;
     for (var i=0; i<this.sliderValue; i++) {
       this.assignCharacterToParty(i);
     }
@@ -58,9 +76,65 @@ export class AppComponent {
     if (this.storedValues.length === this.partySizeLimit) {
       this.errMessage = 'Character swap limit has been met';  
     } else {
+      this.showMyClass = false;
       var idx = this.party.findIndex(party => party.name === name);
       this.assignCharacterToParty(idx);
     }
   }
+  
+  //ngAfterViewChecked()  {
+   // 
+  //}
+  
+  resolve() {
+    this.displayStatic = true;
+  }
+  
+  ngOnChanges() {
+  //  console.log("ngOnChanges");
+  }
 
- }
+  ngOnInit() {
+   // console.log("ngOnInit");
+  }
+
+  ngDoCheck() {
+   // console.log("ngDoCheck");
+  }
+
+  ngAfterContentInit() {
+   // console.log("ngAfterContentInit");
+  }
+
+  ngAfterContentChecked() {
+   //console.log("ngAfterContentChecked");
+  }
+
+  ngAfterViewInit() {
+   // console.log("ngAfterViewInit");
+  }
+
+  ngAfterViewChecked() {
+    //console.log("ngAfterViewChecked");
+    if (this.displayStatic === false) {
+      setTimeout(() => this.resolve(), 5000);
+    }
+  }
+
+  ngOnDestroy() {
+    console.log("ngOnDestroy");
+  }
+
+  mouseEnter(el: Event) {
+    let element: Element = (el.target as Element);
+    element.classList.add('d-none');
+    document.getElementById('imgDynamic0')?.classList.remove('d-none');
+  }
+
+  mouseLeave(el: Event) {
+    console.log('mouseleave');
+    let element: Element = (el.target as Element);
+    element.classList.remove('d-none');
+    document.getElementById('imgDynamic0')?.classList.add('d-none');
+  }
+}
