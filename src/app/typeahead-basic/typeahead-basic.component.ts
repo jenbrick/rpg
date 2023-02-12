b0ebf44fc9b39dda47c7b3ea70859e1dc51b4539
jenbrick <jenbrick73@comcast.net>
2023-02-12 11:43:30 -0500

    swap fix

--- src/app/typeahead-basic/typeahead-basic.component.ts
+++ src/app/typeahead-basic/typeahead-basic.component.ts
@@ -1,73 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
 import { Observable, OperatorFunction } from 'rxjs';
 import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 import { FormsModule } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
-import data from '../../assets/json/characters.json';
 import { AppComponent } from '../app.component';
 
-const states = [
-	'Alabama',
-	'Alaska',
-	'American Samoa',
-	'Arizona',
-	'Arkansas',
-	'California',
-	'Colorado',
-	'Connecticut',
-	'Delaware',
-	'District Of Columbia',
-	'Federated States Of Micronesia',
-	'Florida',
-	'Georgia',
-	'Guam',
-	'Hawaii',
-	'Idaho',
-	'Illinois',
-	'Indiana',
-	'Iowa',
-	'Kansas',
-	'Kentucky',
-	'Louisiana',
-	'Maine',
-	'Marshall Islands',
-	'Maryland',
-	'Massachusetts',
-	'Michigan',
-	'Minnesota',
-	'Mississippi',
-	'Missouri',
-	'Montana',
-	'Nebraska',
-	'Nevada',
-	'New Hampshire',
-	'New Jersey',
-	'New Mexico',
-	'New York',
-	'North Carolina',
-	'North Dakota',
-	'Northern Mariana Islands',
-	'Ohio',
-	'Oklahoma',
-	'Oregon',
-	'Palau',
-	'Pennsylvania',
-	'Puerto Rico',
-	'Rhode Island',
-	'South Carolina',
-	'South Dakota',
-	'Tennessee',
-	'Texas',
-	'Utah',
-	'Vermont',
-	'Virgin Islands',
-	'Virginia',
-	'Washington',
-	'West Virginia',
-	'Wisconsin',
-	'Wyoming',
-];
+
 interface Person { 
   name : String,
   gender : String,
@@ -97,9 +36,8 @@ export class NgbdTypeaheadBasic {
 
   constructor(private appComponent: AppComponent) {}
 	public model: any;
-  origPersons: Person[] = data;
-  
-
+  @Input()
+  index!: number;
   getName(term : Person) {
     return term.name;
   }
@@ -107,13 +45,14 @@ export class NgbdTypeaheadBasic {
     text$.pipe(
       debounceTime(200),
       distinctUntilChanged(),
-      map(term => (this.origPersons.filter(v => v.name.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 100))
+      map(term => (this.appComponent.origPersons.filter(v => v.name.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 100))
     );
 
-   formatter = (x: {name: string}) => x.name;
+  formatter = (x: {name: string}) => x.name;
 
-   replaceCharacter(replaceWith : Person) {
-    this.appComponent.party[0]=replaceWith;
-   }
+  replaceCharacter(replaceWith : Person) {
+    alert(this.index);
+    this.appComponent.party[this.index]=replaceWith;
+  }
 
 }
